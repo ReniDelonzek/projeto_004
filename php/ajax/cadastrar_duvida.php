2ec2bdf96506a7de96a6c105b90125248723e72f
Paulo Cezar Sheliga <paulosheliga@gmail.com>
2020-05-03 22:53:51 -0300

    adição da tela de cadastro de duvida

---
+++ php/ajax/cadastrar_duvida.php
@@ -0,0 +1,20 @@
+<?php
+
+include "base/lib.php";
+$titulo = $_POST['titulo'];
+$duvida = $_POST['duvida'];
+$categoria  = $_POST['materia'];
+$pontos  = $_POST['pontos'];
+$usuario = 1;
+$status = 2;
+
+
+
+
+    
+$duv = new Duvida($titulo, $duvida, $categoria, $usuario, $status, 0);
+$duv -> save();
+
+
+
+?>
