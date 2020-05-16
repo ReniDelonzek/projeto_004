6eed1ac66603a45d697ae834377213b37c822b50
Rogério Portela da Luz <sis-rogeriodaluz@uniguacu.edu.br>
2020-05-16 11:47:44 -0300

    Revert "Update lib.php"
    This reverts commit bd633640bb8397900e206e1e9d791d4d0dd03755.

--- php/base/lib.php
+++ php/base/lib.php
@@ -319,14 +319,12 @@ class Usuario
                 values ('{$this->email}', 
                         '{$this->nome}', 
                         '{$this->senha}',
-                         {$this->pontos_bonificacao} )";
+                        {$this->pontos_bonificacao} )";
                         //{Utils::criptografar($this -> pontos_bonificacao)})";
         $stmt = Conexao::getInstance()->prepare($sql);
-        
-        //verifica se ocorreu algum erro
         if($stmt->execute() === false){
             return $stmt->errorInfo();
-        }else{ 
+        }else{
             return "valido";
         }
     }
