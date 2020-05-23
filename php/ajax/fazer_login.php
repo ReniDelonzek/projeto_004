67e6f3718a7b98c4686db23e2740027de8034d9f
IgorParra <IgorParra@users.noreply.github.com>
2020-05-23 09:56:36 -0300

    Validacao de Sessao

--- php/ajax/fazer_login.php
+++ php/ajax/fazer_login.php
@@ -1,32 +1,17 @@
 <?php
-include_once('../base/lib.php');
-session_start();
-
-$email=$_POST['email'];
-$senha=$_POST['senha'];
-
-$login = new Login();
-if($login -> valida($email,$senha)){
-    $_SESSION['email'] = $email;
-    echo '1';
-}else{
-	echo '0';
-    
-}
-/*
+	include_once('../base/lib.php');
 	session_start();
-	
-    $email=$_POST['email'];
-    $senha=$_POST['senha'];
-    include_once("../base/lib.php");
-    $conexao=new Conexao2;
-    if($conexao->valida($email,$senha) == "valido"){
+
+	$email=$_POST['email'];
+	$senha=$_POST['senha'];
+
+	$login = new Login();
+	if($login -> valida($email,$senha)){
 		$_SESSION['email'] = $email;
-		echo "Valido";
-	}else if($conexao->valida($email,$senha) == "invalido"){
-		echo "Invalido";
+		echo '1';
 	}else{
-		echo "Erro Inesperado";
+		echo '0';
+		
 	}
-*/
+
 ?>
