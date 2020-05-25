25f145cabcf0154b0a111c4a7831c54c3e0da45b
Paulo Sheliga <paulosheliga@gmail.com>
2020-05-24 23:22:18 -0300

    fixx:correção de bugs

--- php/ajax/atualizar_duvida.php
+++ php/ajax/atualizar_duvida.php
@@ -1,41 +1,21 @@
 <?php
-echo "<h1>b</h1>";
-/*
-echo "<div id="cartao_pergunta" class="cartao_pergunta" style="box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);">";
-            <div class="div_pergunta">
-                <label>Faça sua pergunta agora mesmo!</label>
-            </div>
-            <div class="div_icone">
-                <img src="../img/icon.jpeg">
-            </div>
-            <div id="cadastro_duvida" class="div_seletores">
-                <label class="div_cardtexts">Título</label><br>
-                <input id=t itulo type="text" name="titulo" placeholder="Escolhe um título bem chamativo ai!"><br><br>
-                <label class="div_cardtexts">Pergunta</label><br>
-                <input type="text" name="duvida" placeholder="Qual é sua dúvida?"><br>
-            </div> <br>
-            <hr></hr>
-            <div class="div_selects">
-                <label>Vai valer:</label><br>
-                <select name="pontos">
-                    <option>10 pontos</option>
-                    <option>20 pontos</option>
-                    <option>30 pontos</option>
-                </select>
-            </div>
-            <div class="div_selects">
-                <label>Matéria:</label><br>
-                <select id="materia" name="materia">
-                    <option value="1">Robótica</option>
-                    <option value="2"> Programação</option>
-                    <option value="3">Redes</option>
-                </select>
-            </div>
-            <div class="div_selects">
-                <label>Confirmação:</label><br>
-                <input type="submit" value="Alterar" id="alterar" onclick="alterar()">
-            </div>
-        </div>
+    include "../base/lib.php";
+    
+    $id = $_POST['id'];
+    $titulo = $_POST['titulo'];
+    $descricao = $_POST['duvida'];
+    $categoria = $_POST['materia'];
+    $pontos = preg_replace( '/[^0-9]/', '', $_POST['pontos']);
+    
+    
 
-        */
-        ?>
+    $sql = "UPDATE duvida 
+                SET titulo = '{$titulo}', 
+                descricao = '{$descricao}', 
+                categoria_id = {$categoria},
+                pontos = {$pontos}
+                WHERE id = {$id};";
+    
+    $stmt = Conexao::getInstance()->prepare($sql);
+    $stmt->execute();
+?>
